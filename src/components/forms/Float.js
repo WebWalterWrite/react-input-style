fe0951f71af8becec1fdf64e5e05997470c85c19
WebWalterWrite <webwalterwhrite@gmail.com>
2019-11-13 14:26:16 +0100

    :rocket: add new input WestSideFloatInput

--- src/components/forms/Float.js
+++ src/components/forms/Float.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { FloatPlaceHolderInput, OleUpInput } from "../inputs/InputFloat";
+import { FloatPlaceHolderInput, OleUpInput, WestSideFloatInput } from "../inputs/InputFloat";
 import { ContainerForm } from "./form.styled";
 
 const PlaceholderForm = () => {
   return (
     <ContainerForm>
       <div>
-        <h3>ole up input</h3>
+        <h3>fullmoon float input</h3>
         <OleUpInput input="firstname" />
       </div>
 
       <div>
-        <h3>float placeholder input</h3>
+        <h3>west side float input</h3>
+        <WestSideFloatInput input="firstname"/>
+      </div>
+      <div>
+        <h3>noface float input</h3>
         <FloatPlaceHolderInput input="firstname" />
       </div>
     </ContainerForm>
