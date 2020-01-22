17176a797852cd65b4ded62d0d7b382e144d7d14
WebWalterWrite <webwalterwhrite@gmail.com>
2020-01-22 22:56:28 +0100

    type: content desc: add FrameForm component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import CircleForm from './components/forms/Circle';
-import SquareForm from './components/forms/Square';
-import PlaceholderForm from './components/forms/Float';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "./logo.svg";
+import CircleForm from "./components/forms/Circle";
+import SquareForm from "./components/forms/Square";
+import PlaceholderForm from "./components/forms/Float";
+import FrameForm from './components/forms/Frame';
+import { github } from "./utils/icons";
+
 function App() {
   return (
     <>
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React-Input-Farm by</h1>
+        <div className="Header-repo">
+          <a href="*" title="github code"><FontAwesomeIcon icon={github} size="2x" /></a>
+        </div>
+        <div className="Header-title">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>React-Input-Farm</h1>
+        </div>
       </header>
       <main>
         <CircleForm />
         <SquareForm />
         <PlaceholderForm />
+        <FrameForm />
       </main>
     </>
   );
