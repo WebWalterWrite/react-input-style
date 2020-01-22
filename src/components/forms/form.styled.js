819e8acb27cfb03366c7fa0e2e9a7c6b74b978a8
WebWalterWrite <webwalterwhrite@gmail.com>
2020-01-22 22:55:38 +0100

    :lipstick: update style
    modification position des elements

--- src/components/forms/form.styled.js
+++ src/components/forms/form.styled.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components/macro';
 
 export const ContainerForm = styled.section`
+h1{
+  border-bottom: solid 2px #f3dd1a;
+  width: 80%;
+  color: #f3dd1a;
+  margin: 0 auto;
+  padding-bottom: .2em;
+  text-transform: uppercase;
+}
+.Circle-form, .Float-form, .Square-form, .Frame-form{
 display: flex;
 justify-content: space-around;
 align-items: center;
 padding: 1.5em;
+margin-bottom: 2em;
+}
 h3{
   color: #FEFEFEFE;
   text-transform: uppercase;
